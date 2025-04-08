ffdb13a06f804062dd7dfb1cd6aea962ed339b88
OlyaPuh <xotaoshi@gmail.com>
2025-04-08 23:43:12 +0300

    Created save in file func for task 2

---
+++ Homework/StatisticFile.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <stdio.h>
+#include <io.h>
+#include "task2.h"
+
+using namespace std;
+
+struct StatsFile {  
+   const char* filename = "Statistic.txt";  
+
+   FILE* f = nullptr;  
+
+   char* bufffile = nullptr;  
+
+   StatsFile();
+   StatsFile(Statistic& s);
+   ~StatsFile();
+};
+
+
+void ReadableFile(Statistic&s, StatsFile&f);
